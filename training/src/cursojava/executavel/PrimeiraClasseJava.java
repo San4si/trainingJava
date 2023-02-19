643e4da6da85ac6a9f60b32711a5da3b5596a39f
Sandro Ferreira de Brito <soft@gft.com>
2023-02-19 14:11:06 -0300

    outro teste

--- training/src/cursojava/executavel/PrimeiraClasseJava.java
+++ training/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,9 +1,9 @@
 package cursojava.executavel;
 
 public class PrimeiraClasseJava {
-	/*Main é um metodo auto executavel em java*/
+	/*Main é um metodo auto executavel em java - blalalal*/
 	public static void main(String[] args) {
 		System.out.println("Meu primeiro programa em Java - Curso Formação Java Web");
-		System.out.println("Java é orientado a Objetos");
+		System.out.println("Java é bom pra ganhar dinheiro");
 	}
 }
