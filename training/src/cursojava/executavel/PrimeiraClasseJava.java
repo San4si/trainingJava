19b613a7b02540a6ea44135d295bdcbe796cc8fd
Sandro Ferreira de Brito <soft@gft.com>
2023-02-19 13:49:48 -0300

    Crição do projeo

---
+++ training/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,9 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	/*Main é um metodo auto executavel em java*/
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em Java - Curso Formação Java Web");
+		System.out.println("Java é orientado a Objetos");
+	}
+}
---
+++ training/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author soft
+ *
+ */
+module training {
+}
