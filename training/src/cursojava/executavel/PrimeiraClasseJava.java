d93b479adff7f903f4a04230bd0a28e268f580d6
Sandro Ferreira de Brito <soft@gft.com>
2023-03-03 23:34:40 -0300

    Alterando nome da função

--- training/src/cursojava/executavel/PrimeiraClasseJava.java
+++ training/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -1,9 +1,23 @@
 package cursojava.executavel;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class PrimeiraClasseJava {
 	/*Main é um metodo auto executavel em java - blalalal*/
 	public static void main(String[] args) {
-		System.out.println("Meu primeiro programa em Java - Curso Formação Java Web");
-		System.out.println("Java é bom pra ganhar dinheiro");
+		List<Double> numeros = Arrays.asList(4.99,49.92,34.99,54.97,125.00,5.5,28.67,12.5,227.32,13.96,68.29,19.66,25.92);
+		double resultado = somarNumero(numeros);
+		System.out.println("A soma da lista é " + resultado);
+	}
+	
+	public static double somarNumero(List<Double> numeros) {
+		double soma = 0.00;
+		
+		for(double numero : numeros) {
+			soma += numero;
+		}
+		
+		return soma;
 	}
 }
